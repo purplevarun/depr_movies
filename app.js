8ffe042d254fa1573830a98f507e9c82786f5f0b
Varun Kedia <39029055+purplevarun@users.noreply.github.com>
2020-09-21 20:39:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+window.addEventListener("load",()=>{
+
+    //const proxy=`https://cors-anywhere.herokuapp.com/`;
+    var moviename="devil all the time";
+    document.querySelector("#btn").onclick=function(){
+        moviename=document.querySelector("#inp").value;
+        document.querySelector("#inp").value="";
+        document.getElementById("frm").style.display="none";
+    
+        const byname=`http://www.omdbapi.com/?apikey=d25a5f01&t=${moviename}&plot=full&type=movie`;
+        console.log(byname);
+        fetch(byname)
+        .then(response =>{
+            return response.json();
+        })
+        .then(data=>{
+            console.log(data);
+            document.querySelector("#title").innerHTML=data.Title;
+            document.querySelector("#paste").innerHTML=`<img src=${data.Poster} width="100%">`;
+            document.querySelector("#actors").innerHTML="Actors : "+data.Actors;
+            document.querySelector("#director").innerHTML="Director : " + data.Director;
+            document.querySelector("#genre").innerHTML="Genre : " + data.Genre;
+            document.querySelector("#plot").innerHTML="Synopsis : "+data.Plot;
+            document.querySelector("#date").innerHTML="Released in : "+data.Released;
+        })
+    }
+})
